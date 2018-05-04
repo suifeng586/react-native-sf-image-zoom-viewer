76cde726ec4bee8dab473daa08959a8c961fe471
renwei <rw@zhiqusoft.com>
2018-05-04 17:52:12 +0800

    更新格式

--- src/SFZoomViewConfig.js
+++ src/SFZoomViewConfig.js
@@ -3,8 +3,10 @@ export default class SFZoomViewConfig{
 
     //默认图片间距
     static img_dis = 10;
+
     //移动的速度超过多少会更换图片 (0~1.0)
     static max_speed_x = 0.5;
+
     //移动的距离超过多少会更换图片
     static max_move_x = 100;
 
@@ -13,6 +15,7 @@ export default class SFZoomViewConfig{
 
     //是否显示分享按钮
     static is_show_share = true;
+
     //是否开启显示和隐藏动画
     static is_show_ani = true;
 
